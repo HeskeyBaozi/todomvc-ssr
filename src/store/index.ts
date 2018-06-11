e4bd7c278edbf0e423e099886f337a3828145513
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-11 21:00:36 +0800

    update packages

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { Store } from "vuex";
 import todos from "./modules/todos";
-import {ITodosState} from "../interface/index";
+import { ITodosState } from "../interface/index";
 
 Vue.use(Vuex);
 
 export interface IRootState {
-    todos: ITodosState;
+  todos: ITodosState;
 }
 
-export const store: Vuex.Store<IRootState> = new Vuex.Store({
-    modules: {
-        todos
-    }
+export const store: Store<IRootState> = new Vuex.Store({
+  modules: {
+    todos
+  }
 });
